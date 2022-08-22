de877eaa963adf9f1b8442f407d8a1507e604acb
fefilopes <fflopesneto@hotmail.com>
2022-08-22 12:26:47 -0300

    novas aulas

---
+++ expressoes-funcoes.js
@@ -0,0 +1,25 @@
+// declaração de função
+
+function minhaFuncao(param) {
+    // bloco de codigo
+}
+
+minhaFuncao("param");
+
+// expressão de função
+
+//const soma = function(num1, num2) {return num1, num2}
+//console.log(soma(1, 5))
+
+// diferença principal!
+
+console.log(apresentar())
+
+function apresentar() {
+    return "olá";
+}
+
+console.log(soma(1, 5))
+const soma = function(num1, num2) {
+    return num1, num2
+}
